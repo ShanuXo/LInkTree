826fbe36055cd07699f37d635a1e848e0f1df79a
Shanu kumar <shanusah8001@gmail.com>
2024-11-25 02:51:39 +0530

    All Links at one place

---
+++ pages/index.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="h-screen grid grid-cols-3 divide-x">
+    <div class="col-span-2 h-screen flex flex-col bg-slate-100">
+      <div class="flex-1 overflow-y-auto p-8">
+        <app-form-profile
+          v-model:name="data.n"
+          v-model:desc="data.d"
+          v-model:image="data.i"
+        />
+        <app-form-hr />
+        <app-form-social-links
+          v-model:facebook="data.f"
+          v-model:twitter="data.t"
+          v-model:instagram="data.ig"
+          v-model:github="data.gh"
+          v-model:telegram="data.tg"
+          v-model:linkedin="data.l"
+          v-model:email="data.e"
+          v-model:whatsapp="data.w"
+          v-model:youtube="data.y"
+        />
+        <app-form-hr />
+        <app-form-links v-model="data.ls" />
+      </div>
+      <div class="border-t bg-white flex items-center">
+        <button
+          @click="prefillDemoData"
+          class="h-12 flex items-center space-x-2 px-4 border-r text-xs font-medium bg-white text-slate-700"
+        >
+          <span> Add demo data </span>
+          <icon name="mdi:code-json" class="h-4 w-4" />
+        </button>
+        <button
+          @click="publish"
+          class="h-12 flex items-center space-x-2 px-4 border-r text-xs font-medium bg-white text-slate-700"
+        >
+          <span> Publish </span>
+          <icon name="ph:paper-plane-tilt-bold" class="h-4 w-4" />
+        </button>
+        <a
+          href="https://github.com/ShanuXo"
+          target="_blank"
+          class="h-12 flex items-center space-x-2 px-4 border-r text-xs font-medium bg-white text-slate-700"
+        >
+          <span> Github </span>
+          <icon name="mdi:github" class="h-4 w-4" />
+        </a>
+      </div>
+    </div>
+    <app-form-preview :data="data" />
+    <a
+      href="https://x.com/ShanuXo_"
+      target="_blank"
+      class="absolute bottom-0 right-0 bg-white rounded-tl-lg shadow px-4 py-1 font-medium text-sm text-gray-500"
+    >
+    </a>
+  </div>
+</template>
+
+<script setup>
+import { encodeData } from "../utils/transformer";
+const data = ref({
+  n: "",
+  d: "",
+  i: "",
+  f: "",
+  t: "",
+  ig: "",
+  gh: "",
+  tg: "",
+  l: "",
+  e: "",
+  w: "",
+  y: "",
+  ls: [],
+});
+
+const prefillDemoData = () => {
+  data.value = {
+    n: "Shanu Kumar",
+    d: "I’m Software Developer",
+    i: "https://i.insider.com/56743fad72f2c12a008b6cc0",
+    f: "https://www.facebook.com/john_snow",
+    t: "https://x.com/ShanuXo_in",
+    ig: "https://www.instagram.com/https.shanu__/",
+    e: "shanusah8001@gmail.com",
+    gh: "https://github.com/ShanuXo",
+    tg: " ",
+    w: "+919304641125",
+    y: "https://www.youtube.com/@Shanu.http_04",
+    l: "https://www.linkedin.com/in/shanuxo04/",
+    ls: [
+      {
+        l: "My Website",
+        i: "ph:globe-duotone",
+        u: "https://shanukumarxo.netlify.app/",
+      },
+      {
+        l: "Topmate.io",
+        i: "ant-design:amazon-outlined",
+        u: "https://topmate.io/shanu_kumar04",
+      },
+      {
+        l: "Leetcode",
+        i: "grommet-icons:reactjs",
+        u: "https://leetcode.com/u/ShanuXo/",
+      },
+      {
+        l: "GeeksForGeeks",
+        i: "iconoir:donate",
+        u: "https://www.geeksforgeeks.org/user/shanusah8001/",
+      },
+    ],
+  };
+};
+
+const publish = () => {
+  const url = `${window.location.origin}/1?data=${encodeData(data.value)}`;
+  navigator.clipboard.writeText(url).then(() => {
+    alert("Link copied to clipboard");
+  });
+};
+</script>
